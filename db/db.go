cb27c7e66a63a44c48db5ec9a550030b65db9032
John Ayilara <jbayilara@gmail.com>
2024-05-15 13:25:55 +0100

    added prod database

--- db/db.go
+++ db/db.go
@@ -3,8 +3,10 @@ package db
 import (
 	"context"
 	"database/sql"
+	"fmt"
 	"os"
 
+	_ "github.com/lib/pq"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/redis/go-redis/v9"
 )
@@ -15,12 +17,11 @@ var RDB *redis.Client
 
 func InitDatabase() {
 	initRedis()
-	Database, err := sql.Open("sqlite3", "api.db")
-	if err != nil {
-		panic(err)
+	if os.Getenv("ENV") == "production" {
+		initPostgres()
+	} else {
+		initSqlite()
 	}
-
-	MainDB = Database
 	MainDB.SetMaxOpenConns(14)
 	MainDB.SetMaxIdleConns(7)
 	CreateTables()
@@ -34,4 +35,20 @@ func initRedis() {
 	})
 	RDB = rdb
 
+}
+func initPostgres() {
+	connStr := fmt.Sprintf("user=%s password=%s host=%s dbname=%s", os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_DB"))
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		panic(err)
+	}
+	MainDB = db
+}
+
+func initSqlite() {
+	db, err := sql.Open("sqlite3", "api.db")
+	if err != nil {
+		panic(err)
+	}
+	MainDB = db
 }
